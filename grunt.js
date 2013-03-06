400ff248cf4893eeff5b8075c9a1017b51fe0fea
Jack Viers <jackcviers@gmail.com>
2013-03-06 16:16:53 -0600

    Finishes grunt build definition

--- grunt.js
+++ grunt.js
@@ -5,13 +5,32 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: '<json:package.json>',
     meta: {
-      /*
-       * 
-       * 
-       */
       banner: '/*******************************************************************************\n'
-	      + 'Copyright (c) 2013 Banno LLC. All rights reserved.\n'+ 
-	    '*******************************************************************************/'
+	+ '# Chosen, a Select Box Enhancer for jQuery and Protoype # by Patrick\n'+ 
+	'#Filler for [Harvest](http://getharvest.com)\n'+ 
+	'\n'+ 
+	'Available for use under the [MIT License](http://en.wikipedia.org/wiki/MIT_License)\n'+ 
+	'\n'+ 
+	'Copyright (c) 2011 by Harvest\n'+ 
+	'\n'+ 
+	'Permission is hereby granted, free of charge, to any person obtaining a copy\n'+ 
+	'of this software and associated documentation files (the "Software"), to deal\n'+ 
+	'in the Software without restriction, including without limitation the rights\n'+ 
+	'to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n'+ 
+	'copies of the Software, and to permit persons to whom the Software is\n'+ 
+	'furnished to do so, subject to the following conditions:\n'+ 
+	'\n'+ 
+	'The above copyright notice and this permission notice shall be included in\n'+ 
+	'all copies or substantial portions of the Software.\n'+ 
+	'\n'+ 
+	'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n'+ 
+	'IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n'+ 
+	'FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n'+ 
+	'AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n'+ 
+	'LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n'+ 
+	'OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\n'+ 
+	'THE SOFTWARE.\n'+ 
+	'*******************************************************************************/'
     },
     lint: {
       files: ['grunt.js', 'src/main/coffeescript/*.coffee', 'src/test/coffeescript/*.coffee']
@@ -33,17 +52,17 @@ module.exports = function(grunt) {
       tasks: 'browserify concat mocha'
     },
     browserify: {
-      'lib/americano.js': {
+      'lib/chosen-jquery-browserify.js': {
         aliases: ['jquery:jquery-browserify'],
-        entries: ["src/main/coffeescript/americano.coffee", "src/main/javascript/chosen.js"],
+        entries: ["src/main/coffeescript/chosen-jquery-browserify.coffee"],
         prepend: ['<banner:meta.banner>'],
 	dest: 'lib/<%= pkg.name %>.js'
       },
-      'testable': {
+      'lib/testable': {
         aliases: ['jquery:jquery-browserify'],
-	entries: ["src/main/coffeescript/*.coffee", "src/test/coffeescript/*.coffee", "src/main/javascript/chosen.js"],
+	entries: ["src/main/coffeescript/*.coffee", "src/test/coffeescript/*.coffee"],
         prepend: ['<banner:meta.banner>'],
-	dest: 'test/americano-specs.js'
+	dest: 'test/chosen-specs.js'
       }
     },
     mocha: {
@@ -51,7 +70,7 @@ module.exports = function(grunt) {
         src: ['test/**/*.html'],
         options: {
           ignoreLeaks: true,
-          globals: ['Americano', 'jQuery', '$', 'window', 'document'],
+          globals: ['window', 'document'],
           ui: 'bdd',
           run: true,
           colors: true,
@@ -62,27 +81,32 @@ module.exports = function(grunt) {
     coffee: {
       compile: {
 	files: {
-	  'lib/compiled.js': ['src/main/coffeescript/*.coffee', 'src/main/coffeescript/**/*.coffee'],
-	  'lib/test-compiled.js' : ['src/test/coffeescript/*.coffee', 'src/test/coffeescript/**/*.coffee']
+	  'lib/compiled.js': ['src/main/coffeescript/*.coffee'],
+	  'lib/test-compiled.js' : ['src/test/coffeescript/*.coffee']
 	},
 	options: {
 	  map: true
 	}
       }	
     },
-    handlebars: {
-      compile: {
+    copy: {
+      dist: {
+	files:{
+	"dist/resources/chosen-sprite@2x.png": ["src/main/resources/chosen-sprite@2x.png"],
+	"dist/resources/chosen-sprite.png": ["src/main/resources/chosen-sprite.png"],
+	"test/resources/chosen-sprite@2x.png": ["src/main/resources/chosen-sprite@2x.png"],
+	"test/resources/chosen-sprite.png": ["src/main/resources/chosen-sprite.png"]}
+      }
+    },
+    mincss: {
+      compress: {
 	options: {
-	  wrapped: true,
-	  namespace: "com.banno.templates",
-	  node: true,
-	  processName: function(filename) {
-	    var filenameRegex = /(?:.*\/)*(.*)(?:\.handlebars$)/
-	    return filenameRegex.exec(filename)[1];
-	  }
+	  banner: '<banner:meta.banner>'
 	},
 	files: {
-	  "lib/templates.js": ["src/main/handlebars/*.handlebars", "src/main/handlebars/**/*.handlebars", "src/main/handlebars/**/**/*.handlebars", "src/main/handlebars/**/**/**/*.handlebars"]
+	  "dist/resources/chosen.min.css": ["src/main/resources/chosen.css"],
+	  "test/resources/mocha.min.css": ["node_modules/grunt-mocha/example/test/css/mocha.css"],
+	  "test/resources/chosen.min.css": ["src/main/resources/chosen.css"]
 	}
       }
     },
@@ -90,15 +114,13 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-mocha');
   grunt.loadNpmTasks('grunt-browserify');
-  // coffee grunt plugin needed to detect coffeescript errors and bail
-  // if those errors exist.
-  // need to figure out how to include handlebars helpers and what not, 
-  // and how to add the handlebars compiled templates to the build, then
-  // we are done!!!!
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
   // Default task.
-  grunt.registerTask('test', 'handlebars coffee browserify concat mocha');
-  grunt.registerTask('default', 'handlebars coffee browserify concat min');
+  grunt.registerTask('test', 'coffee browserify mincss copy concat mocha');
+  grunt.registerTask('default', 'coffee browserify mincss copy concat min');
 
 };
